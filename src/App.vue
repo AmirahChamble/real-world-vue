bf1cf2ff1696f7ed5213301b0466d0fd8cbdb59c
Amirah Chamble <amirahchamble@Amirahs-MBP.fios-router.home>
2018-12-26 15:25:03 -0500

    feat: added some dynamic routes

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
+/* eslint-disable */
 <template>
   <div id="app">
+
     <div id="nav">
-      <router-link :to="{ name: 'event-list'}" > List </router-link> |
-      <router-link :to="{ name: 'event-show' }" > Show Event # 1 </router-link> |
-      <router-link :to="{ name: 'event-create'}" > Create Event </router-link> 
+
+      <router-link 
+        :to="{ name: 'event-list'}" 
+      > List </router-link> |
+
+      <router-link 
+        :to="{ name: 'event-create'}" 
+      > Create Event </router-link> |
+
+      <router-link 
+        :to="{ name: 'user', params: {username: 'Amirah'} }" 
+      > Amirah </router-link> 
+
     </div>
+
     <router-view />
+
   </div>
 </template>
 
